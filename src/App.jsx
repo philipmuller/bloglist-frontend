0fde4ee50d43d9a710f2c672774d318269a01d02
Philip Müller <philipmuller97@gmail.com>
2024-05-10 18:07:39 +0300

    Complete 5.2: Blog List Frontend, step 2

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,28 @@ const App = () => {
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
+    )
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
   }, [])
 
   const handleLogin = async ({username, password}) => {
     const authUser = await loginService.login({ username, password })
     if (authUser) {
       setUser(authUser)
+      window.localStorage.setItem('loggedUser', JSON.stringify(authUser))
     }
+  }
+
+  const handleLogout = () => {
+    setUser(null)
+    window.localStorage.removeItem('loggedUser')
   }
 
   if (user === null) {
@@ -30,7 +44,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged-in</p>
+      <p>{user.name} logged-in <button onClick={handleLogout}>Log out</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
