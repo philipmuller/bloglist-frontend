883c4b3748a0ea432977047a2376b4db19b31154
Philip Müller <philipmuller97@gmail.com>
2024-05-10 22:41:55 +0300

    Solve 5.9: Blog List Frontend, step 9, 5.10: Blog List Frontend, step 10

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,7 @@ const App = () => {
       <p>{user.name} logged-in <button onClick={handleLogout}>Log out</button></p>
       <CreateBlog onCreate={handleCreateBlog}/>
       <div>
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} onLikeBlog={handleLikeBlog} />
       )}
       </div>
