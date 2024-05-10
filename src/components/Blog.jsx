75b9223eb8021e98edb6d619e7e7c17736c64af0
Philip Müller <philipmuller97@gmail.com>
2024-05-10 20:55:09 +0300

    Solve 5.7 Blog List Frontend, step 7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,26 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} || {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [detailShown, setDetailShown] = useState(false)
+
+  const toggleDetail = () => {
+    setDetailShown(!detailShown)
+  }
+
+  const details = () => (
+    <div>
+      <div>{blog.url}</div>
+      <div>{blog.likes} likes <button>like</button></div>
+      <div>added by {blog.user.name}</div>
+    </div>
+  )
+
+  return (
+    <div className="blog">
+      <h3>{blog.title} by {blog.author} <button onClick={toggleDetail}>{detailShown ? "hide details" : "view details"}</button></h3>
+      {detailShown && details()}
+    </div>  
+  )
+}
 
 export default Blog
